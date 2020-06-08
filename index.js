8ac1a72a54336a4ee139b941c9807175ca97b77e
Ruiz Lucas <lucas.ruiz@grupomost.com>
2020-06-08 18:06:56 -0300

    master

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extends: true }));
 
 //puerto del app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -23,6 +23,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 //arrancar app
-app.listen(PORT, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
